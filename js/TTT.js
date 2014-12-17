cf1e85b8d65277563c3ab7e89d08b6968f3cbbfc
mariano89 <marianoe1989@gmail.com>
2014-12-17 14:16:00 -0800

    Made tiles clickable
    [#84808180]

--- js/TTT.js
+++ js/TTT.js
@@ -1,13 +1,18 @@
 var TicTacToeApp = angular.module("TicTacToeApp", []);
 
-TicTacToeApp.controller("myController", ['$scope', function($scope){
+TicTacToeApp.controller("TTTController", ['$scope', function($scope){
 
 	
+$scope.tiles = [
+		[{owner:""},{owner:""},{owner:""}],
+		[{owner:""},{owner:""},{owner:""}],
+		[{owner:""},{owner:""},{owner:""}]
+	];
 
+	$scope.click = function(){
+		console.log($scope.tiles[0][0].cellState == "X" && $scope.grid[0][1].cellState == "X" && $scope.tiles[0][2].cellState == "X")
+		alert("CLICK!!");
+	};
+	
 
-
-
-
-
-
-}])
+}]);
