96094f12db1f9eade1323925385ad01694a86b91
mariano89 <marianoe1989@gmail.com>
2015-01-03 15:15:39 -0800

    added firebase
    [#85378602]

--- js/tttController.js
+++ js/tttController.js
@@ -12,17 +12,16 @@ function TTTController($firebase) {
 	self.playerOne = new Player("Playe One");
 	self.playerTwo = new Player("Player Two");
 	//self.playersTie = false;
-	self.tiles = [];
 	self.currentTurn = "X";
 	self.players.ties = 0;
-	self.marianottt = marianottt();
+	self.marianottt = marianottt(); 
+	self.marianottt.tiles = [];
 
-
-		// ************** Connection to Firebase ************
+				// ************** Connection to Firebase ************
 	function marianottt(){
 		var ref = new Firebase('https://marianottt.firebaseio.com/');
 		var mttt = $firebase(ref).$asObject();
-		console.log(mttt);
+		//console.log(mttt);
 		return mttt;
 	}
 
@@ -32,50 +31,49 @@ function TTTController($firebase) {
 		this.wins = 0;
 	}
 
-
-	      // ********** Array Constructor ***********
+	   		   // ********** Array Constructor **************
     (function tilePusher(){
     	for(var i = 0; i < 9; i++){
-    		self.tiles.push(new tile(i));
+    		self.marianottt.tiles.push(new tile(i));
     	}	self.marianottt.$save();
     })();
 
 
     function tile(position) {
-    	//console.log(position);
     	this.position = position;
     	this.players = '';
     	this.playerOne = false;
     	this.playerTwo = false;
-
+	}
 
 			// ********* Click Tiles **********
-    	this.click = function() {
+	 
+	self.click = function($index){
+		if (self.marianottt.tiles[$index].players === ''){
+			if(self.currentTurn == "X"){
+				self.marianottt.tiles[$index].players = "X";
+				p1WinCheck();
+				if ( self.playerOneWon === false ){
+					tieCheck();
+				}
+				return self.currentTurn = "O";
+			} else if (self.currentTurn == "O"){
+				self.marianottt.tiles[$index].players = "O";
+				p2WinCheck();
+				if (self.playerTwoWon === false){
+					tieCheck();
+				}
+					return self.currentTurn = "X";
+			}
+		}
+		self.marianottt.$save();
+	}
 
-	     	if(this.players === ''){
-	     		if(self.currentTurn == "X"){
-	     			this.players = "X";
-	     			p1WinCheck();
-	     			if (self.playerOneWon === false){
-	     			tieCheck();
-	     		}
-	     			return self.currentTurn = "O";
-	     		} else if (self.currentTurn == "O"){
-	     			this.players = "O";
-	     			p2WinCheck();
-	     			if (self.playerTwoWon === false){
-	     			tieCheck();
-	     		}
-	     			return self.currentTurn = "X";
-	     		}
-	     	}
-
-	     }; self.marianottt.$save();
-	 }
-	     // ******* New Game Clears Tiles ********
+	     	// ******* New Game Clears Tiles ********
+	
 	function newGame() {
 		for(var i = 0; i < 9; i++){
-			self.tiles[i].players = '';
+			self.marianottt.tiles[i].players = '';
 		}
 		 self.marianottt.$save();
 	}
@@ -84,7 +82,7 @@ function TTTController($firebase) {
 	function tieCheck(){
 		var check = 0;
 		for(i = 0; i < 9; i++){
-			if(!self.tiles[i].players == ""){
+			if(!self.marianottt.tiles[i].players == ""){
 				check++;
 			}
 		} 
@@ -99,17 +97,17 @@ function TTTController($firebase) {
 		}
 
 
-
+ 			// ********* Checks for winning possibilities ***************
 
 	function p1WinCheck() {
-		if((self.tiles[0].players == "X" && self.tiles[1].players == "X" && self.tiles[2].players == "X") ||
-			(self.tiles[3].players == "X" && self.tiles[4].players == "X" && self.tiles[5].players == "X") ||
-			(self.tiles[6].players == "X" && self.tiles[7].players == "X" && self.tiles[8].players == "X") ||
-			(self.tiles[0].players == "X" && self.tiles[3].players == "X" && self.tiles[6].players == "X") ||
-			(self.tiles[1].players == "X" && self.tiles[4].players == "X" && self.tiles[7].players == "X") ||
-			(self.tiles[2].players == "X" && self.tiles[5].players == "X" && self.tiles[8].players == "X") ||
-			(self.tiles[0].players == "X" && self.tiles[4].players == "X" && self.tiles[8].players == "X") ||
-			(self.tiles[2].players == "X" && self.tiles[4].players == "X" && self.tiles[6].players == "X")) {
+		if((self.marianottt.tiles[0].players == "X" && self.marianottt.tiles[1].players == "X" && self.marianottt.tiles[2].players == "X") ||
+			(self.marianottt.tiles[3].players == "X" && self.marianottt.tiles[4].players == "X" && self.marianottt.tiles[5].players == "X") ||
+			(self.marianottt.tiles[6].players == "X" && self.marianottt.tiles[7].players == "X" && self.marianottt.tiles[8].players == "X") ||
+			(self.marianottt.tiles[0].players == "X" && self.marianottt.tiles[3].players == "X" && self.marianottt.tiles[6].players == "X") ||
+			(self.marianottt.tiles[1].players == "X" && self.marianottt.tiles[4].players == "X" && self.marianottt.tiles[7].players == "X") ||
+			(self.marianottt.tiles[2].players == "X" && self.marianottt.tiles[5].players == "X" && self.marianottt.tiles[8].players == "X") ||
+			(self.marianottt.tiles[0].players == "X" && self.marianottt.tiles[4].players == "X" && self.marianottt.tiles[8].players == "X") ||
+			(self.marianottt.tiles[2].players == "X" && self.marianottt.tiles[4].players == "X" && self.marianottt.tiles[6].players == "X")) {
 			alert(self.playerOne.name + " Wins");
 			self.playerOne.wins++;
 			self.playerOneWon = true;
@@ -122,14 +120,14 @@ function TTTController($firebase) {
 }
 
 	function p2WinCheck() {
-		if((self.tiles[0].players == "O" && self.tiles[1].players == "O" && self.tiles[2].players == "O") ||
-			(self.tiles[3].players == "O" && self.tiles[4].players == "O" && self.tiles[5].players == "O") ||
-			(self.tiles[6].players == "O" && self.tiles[7].players == "O" && self.tiles[8].players == "O") ||
-			(self.tiles[0].players == "O" && self.tiles[3].players == "O" && self.tiles[6].players == "O") ||
-			(self.tiles[1].players == "O" && self.tiles[4].players == "O" && self.tiles[7].players == "O") ||
-			(self.tiles[2].players == "O" && self.tiles[5].players == "O" && self.tiles[8].players == "O") ||
-			(self.tiles[0].players == "O" && self.tiles[4].players == "O" && self.tiles[8].players == "O") ||
-			(self.tiles[2].players == "O" && self.tiles[4].players == "O" && self.tiles[6].players == "O")) {
+		if((self.marianottt.tiles[0].players == "O" && self.marianottt.tiles[1].players == "O" && self.marianottt.tiles[2].players == "O") ||
+			(self.marianottt.tiles[3].players == "O" && self.marianottt.tiles[4].players == "O" && self.marianottt.tiles[5].players == "O") ||
+			(self.marianottt.tiles[6].players == "O" && self.marianottt.tiles[7].players == "O" && self.marianottt.tiles[8].players == "O") ||
+			(self.marianottt.tiles[0].players == "O" && self.marianottt.tiles[3].players == "O" && self.marianottt.tiles[6].players == "O") ||
+			(self.marianottt.tiles[1].players == "O" && self.marianottt.tiles[4].players == "O" && self.marianottt.tiles[7].players == "O") ||
+			(self.marianottt.tiles[2].players == "O" && self.marianottt.tiles[5].players == "O" && self.marianottt.tiles[8].players == "O") ||
+			(self.marianottt.tiles[0].players == "O" && self.marianottt.tiles[4].players == "O" && self.marianottt.tiles[8].players == "O") ||
+			(self.marianottt.tiles[2].players == "O" && self.marianottt.tiles[4].players == "O" && self.marianottt.tiles[6].players == "O")) {
 			alert(self.playerTwo.name + " Wins");
 			self.playerTwo.wins++;
 			self.playerTwoWon = true;
