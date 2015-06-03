3848075b9aeeb5b1ff5b384aec27021050c9daa4
mariano89 <marianoe1989@gmail.com>
2015-06-02 17:04:17 -0700

    scrolling chat

--- js/chat.js
+++ js/chat.js
@@ -1,14 +1,21 @@
 $(document).ready(function(){
-	$('.sendMessage').on('click', function(){
-		if($('.chatInput').val() == ''){
-			$(this).attr('disabled', true);
+	$('.sendMessage').prop('disabled', true);
+	
+	$('.chatInput').keyup(function(){
+		if($('.chatInput').val().length != 0){
+			$('.sendMessage').prop('disabled', false);
+		} else {
+			$('.sendMessage').prop('disabled', true);
 		}
+	});
+
+	$('.sendMessage').on('click', function(){
 		var x = $(this).siblings('.chatInput').val();
 		// console.log(x);
 		$('.chatDisplay').append(x + "<hr>");
 		$('.chatInput').val('');
+		$('.sendMessage').prop('disabled', true);
 	});
-	$('body').css('background-color', 'red');
 });
 
 // function chatty(){
